112bd9c356ac10c8af92ecb7d99df82445b57d74
theboymo <billfero@gmail.com>
2017-10-20 20:06:57 +0100

    impl microposts model and controllers via rails scaffolding

--- Rails-Tutorial/toy_app_rspec/db/schema.rb
+++ Rails-Tutorial/toy_app_rspec/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020165050) do
+ActiveRecord::Schema.define(version: 20171020182958) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
