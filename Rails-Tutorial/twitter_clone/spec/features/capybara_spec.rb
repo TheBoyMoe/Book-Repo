1be53e5835c92e75578d058c34718dddf04e1a4a
theboymo <billfero@gmail.com>
2017-10-22 09:56:23 +0100

    refactor capybara specs removing duplicate title

--- Rails-Tutorial/twitter_clone/spec/features/capybara_spec.rb
+++ Rails-Tutorial/twitter_clone/spec/features/capybara_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 describe "testing using capybara" do
+  before(:each) do
+    @base_title = 'Twitter Clone App'
+  end
 
   context "GET 'static/home'" do
     before(:each) {visit '/static/home'}
 
     it "has a title element with the text 'Home | Twitter Clone App'" do
-      expect(page.body).to include("<title>Home | Twitter Clone App</title>")
+      expect(page.body).to include("<title>Home | #{@base_title}</title>")
       find('h1', text: 'Sample App')
     end
   end
@@ -15,7 +18,7 @@ describe "testing using capybara" do
     before(:each) {visit 'static/help'}
 
     it "has a title tag with the text 'Help | Twitter Clone App'" do
-      expect(page.body).to include("<title>Help | Twitter Clone App</title>")
+      expect(page.body).to include("<title>Help | #{@base_title}</title>")
       find('h1', text: 'Help Page')
     end
   end
@@ -24,7 +27,7 @@ describe "testing using capybara" do
     before(:each) {visit 'static/about'}
 
     it "has a title tag with the text 'About | Twitter Clone App'" do
-      expect(page.body).to include("<title>About | Twitter Clone App</title>")
+      expect(page.body).to include("<title>About | #{@base_title}</title>")
       find('h1', text: 'About Page')
     end
   end
