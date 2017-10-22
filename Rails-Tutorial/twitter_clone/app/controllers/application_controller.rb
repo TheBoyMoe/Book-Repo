7066d34ca5d3a6ce1e53cb814a465b06babb42d1
theboymo <billfero@gmail.com>
2017-10-22 12:14:33 +0100

    change root route to home page

--- Rails-Tutorial/twitter_clone/app/controllers/application_controller.rb
+++ Rails-Tutorial/twitter_clone/app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: 'Nothing to see here - just another Rails 5 app!'
-  end
 end
--- Rails-Tutorial/twitter_clone/spec/views/static/home.html.erb_spec.rb
+++ Rails-Tutorial/twitter_clone/spec/views/static/home.html.erb_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "static/home.html.erb", type: :view do
 
+  describe "GET '/'", :type => :feature do
+    before(:each) do
+      @base_title = 'Twitter Clone App'
+      visit '/'
+    end
+
+    it "has a title element with the text 'Home | Twitter Clone App' and a page heading" do
+      expect(page.body).to include("<title>Home | #{@base_title}</title>")
+    end
+  end
+
   describe "GET 'static/home'", :type => :feature do
     before(:each) do
       @base_title = 'Twitter Clone App'
