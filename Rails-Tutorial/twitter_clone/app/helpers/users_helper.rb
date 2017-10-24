831e53938b0f2fc55b6314911a47c9919d02300a
Bill Fero <billfero@gmail.com>
2017-10-24 18:40:12 +0100

    add size parameter to gravatar helper method allowing the gravatar image to be resized

--- Rails-Tutorial/twitter_clone/app/helpers/users_helper.rb
+++ Rails-Tutorial/twitter_clone/app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
 
   # returns the gravatar for a given user
-  def gravatar_for(user)
+  def gravatar_for(user, size = '80')
     # gravatar's are based on the MD5 hash of a users email
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", width: size)
   end
 end
