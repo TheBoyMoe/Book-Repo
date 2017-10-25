87fe5fff61cbe757f5c469e00ce42f39811c641c
theboymo <billfero@gmail.com>
2017-10-25 20:41:22 +0100

    impl login form

--- Rails-Tutorial/twitter_clone/spec/views/sessions/new.html.erb_spec.rb
+++ Rails-Tutorial/twitter_clone/spec/views/sessions/new.html.erb_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "sessions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "login", type: :feature do
+    before(:each){visit login_path}
+
+    it "has a signup form that posts to 'login' and accepts an email and password" do
+      expect(page).to have_selector('form[action="/login"]')
+      expect(page).to have_field('session_email')
+      expect(page).to have_field('session_password')
+    end
+
+    it "has a 'Sign up now!' link that redirects users to the signup page" do
+      click_link 'Sign up now!'
+
+      expect(page.body).to include(full_title('Signup'))
+      expect(page.body).to have_selector('form[action="/signup"]')
+    end
+
+  end
 end
