d45bf4be25a23e11984256faa4cabb7fd4e8f34f
theboymo <billfero@gmail.com>
2017-10-26 14:07:07 +0100

    session#create action displays a flash message if login fails

--- Rails-Tutorial/twitter_clone/app/controllers/sessions_controller.rb
+++ Rails-Tutorial/twitter_clone/app/controllers/sessions_controller.rb
@@ -5,6 +5,14 @@ class SessionsController < ApplicationController
 
   def create
     # log the user in by creating the session
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log the user in & redirect to user's show page
+    else
+      # return an error - use 'flash.now' for rendered views - contents disappear as soon as there's an additional request.
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
   end
 
   def destroy
