a7eb63cc7f24e8c35fa481f98c6ee77036d57139
theboymo <billfero@gmail.com>
2017-10-31 18:05:20 +0000

    send out reset email with the password reset link

--- Rails-Tutorial/twitter_clone/app/models/user.rb
+++ Rails-Tutorial/twitter_clone/app/models/user.rb
@@ -61,7 +61,7 @@ class User < ApplicationRecord
      UserMailer.account_activation(self).deliver_now
   end
 
-  # Sets the password reset attributes.
+  # create the reset digest and update the user record
   def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
