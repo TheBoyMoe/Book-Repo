6419f7aebb4bb45051cae80c7eb1afe54749b0a7
theboymo <billfero@gmail.com>
2017-10-31 18:49:46 +0000

    render reset password edit form

--- Rails-Tutorial/twitter_clone/app/controllers/password_resets_controller.rb
+++ Rails-Tutorial/twitter_clone/app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+
   def new
     # automatically renders views/password_resets/new.html.erb
   end
@@ -17,6 +20,19 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  # update the user with the new password
   def edit
+
   end
+
+  private
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    def valid_user
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        redirect_to root_path
+      end
+    end
 end
