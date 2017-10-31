0592c6caf16573ba0ce08ae87fbc0eeddb568db1
theboymo <billfero@gmail.com>
2017-10-31 17:40:05 +0000

    expand the user model to include reset_digest

--- Rails-Tutorial/twitter_clone/app/controllers/password_resets_controller.rb
+++ Rails-Tutorial/twitter_clone/app/controllers/password_resets_controller.rb
@@ -1,5 +1,20 @@
 class PasswordResetsController < ApplicationController
   def new
+    # automatically renders views/password_resets/new.html.erb
+  end
+
+  # create reset_token and reset_digest
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render 'new'
+    end
   end
 
   def edit
