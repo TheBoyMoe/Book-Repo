b298205ac3c77146c38bd776b053589da8f2e2dc
theboymo <billfero@gmail.com>
2017-11-02 20:46:59 +0000

    impl image uploading and adding to microposts

--- Rails-Tutorial/twitter_clone/app/models/micropost.rb
+++ Rails-Tutorial/twitter_clone/app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> {order(created_at: :desc)} # => newest -> oldest (latest first)
+  mount_uploader :picture, PictureUploader # => associate CarrierWave's uploader class with the model
+
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 140}
 end
