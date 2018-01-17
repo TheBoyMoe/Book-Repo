6d699637b08ed05df1d7845129a4532a991eb9de
theboymo <billfero@gmail.com>
2018-01-17 12:00:16 +0000

    set the failing spec to pending

--- Effective-Testing-with-RSpec3/projects/expense-tracker/app/api.rb
+++ Effective-Testing-with-RSpec3/projects/expense-tracker/app/api.rb
@@ -5,9 +5,14 @@ module ExpenseTracker
 
 	class API < Sinatra::Base
 
-		# add route that handles 'expense' requests
+		# route that handles submitting 'expense'
 		post '/expenses' do
 			JSON.generate('expense_id' => 10)
+		end
+
+		# route that handles fetching expenses by date
+		get '/expenses/:date' do
+			JSON.generate([])
 		end
 
 	end
