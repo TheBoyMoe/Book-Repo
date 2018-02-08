82cc5db72d4ae6f4a78d0611f7c37254941cc9f7
theboymo <billfero@gmail.com>
2018-02-08 20:20:52 +0000

    impl rspec install and config

--- everydayrails-rspec-2017/config/application.rb
+++ everydayrails-rspec-2017/config/application.rb
@@ -14,5 +14,17 @@ module Projects
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # generate only controller and model specs when running the generators
+    # fixtures -> we'll enable when we test factories
+    # view_specs -> we'll test through feature/integration specs
+    # helper/routing_specs -> test in larger apps
+    config.generators do |generator|
+      generator.test_framework :rspec,
+         fixtures: false,
+         view_specs: false,
+         helper_specs: false,
+         routing_specs: false
+    end
   end
 end
