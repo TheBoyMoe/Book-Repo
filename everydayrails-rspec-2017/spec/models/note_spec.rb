a8fe628f5823e5ea8cf871de1262fe0dab60f2c4
theboymo <billfero@gmail.com>
2018-02-09 15:05:50 +0000

    test note validations

--- everydayrails-rspec-2017/spec/models/note_spec.rb
+++ everydayrails-rspec-2017/spec/models/note_spec.rb
@@ -2,19 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
 
-	# use describe to outline general functionality or feature
-	describe 'search note for a term' do
+	before {
+    @user = User.create(
+        first_name: 'Joe',
+        last_name: 'Smith',
+        email: 'joe@ex.com',
+        password: 'password'
+    )
+    @project = @user.projects.create(name: 'project 1')
+  }
+
+	it 'is valid with a user, project and message' do
+		note = Note.create(message: 'New Note', user: @user, project: @project)
+
+		expect(note).to be_valid
+	end
+
+	it 'is invalid without a message' do
+		note = Note.new(message: nil)
+		note.valid?
+
+		expect(note.errors[:message]).to include("can't be blank")
+	end
+
+  # use describe to outline general functionality or feature
+  describe 'search note for a term' do
     before {
-      user = User.create(
-          first_name: 'Joe',
-          last_name: 'Smith',
-          email: 'joe@ex.com',
-          password: 'password'
-      )
-      project = user.projects.create(name: 'project 1')
-      @note_1 = project.notes.create(message: 'note 1, with some dummy text', user: user)
-      @note_2 = project.notes.create(message: 'note 2, with even more text', user: user)
-      @note_3 = project.notes.create(message: 'note 3, just add the dummy', user: user)
+      @note_1 = @project.notes.create(message: 'note 1, with some dummy text', user: @user)
+      @note_2 = @project.notes.create(message: 'note 2, with even more text', user: @user)
+      @note_3 = @project.notes.create(message: 'note 3, just add the dummy', user: @user)
     }
 
     # use context to outline a specific state
@@ -31,7 +47,7 @@ RSpec.describe Note, type: :model do
       # sad path
       it 'returns an empty collection when no results are found' do
         expect(Note.search('content')).to be_empty
-				# expect(Note.search('text')).to_not be_empty # DEBUG tests before block
+				expect(Note.search('text')).to_not be_empty # DEBUG tests before block
       end
 		end
 
