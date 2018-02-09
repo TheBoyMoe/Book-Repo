024b5f078821c03392dc9b7c19d7637c8c8edcb8
theboymo <billfero@gmail.com>
2018-02-09 14:02:49 +0000

    check that a user cannot have two projects of the same name

--- everydayrails-rspec-2017/spec/models/project_spec.rb
+++ everydayrails-rspec-2017/spec/models/project_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it 'does not allow duplicate project names per user'
+	before {
+		@user = User.create(
+				first_name: 'Joe',
+				last_name: 'Smith',
+				email: 'joe@ex.com',
+				password: 'password'
+		)
+		@user.projects.create(name: 'Web Development with Rails')
+	}
 
-	it 'allows two users to share a project name'
+	it 'does not allow duplicate project names per user' do
+		duplicate = @user.projects.build(name: 'Web Development with Rails')
+		duplicate.valid?
+
+		expect(duplicate.errors[:name]).to include('has already been taken')
+	end
+
+	it 'allows two users to share a project name' do
+		other_user = User.create(
+				first_name: 'Tom',
+				last_name: 'Smith',
+				email: 'tom@ex.com',
+				password: 'password'
+		)
+		other_project = other_user.projects.build(name: 'Web Development with Rails')
+
+		expect(other_project).to be_valid
+	end
 end
