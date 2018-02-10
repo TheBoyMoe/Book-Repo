aa673f472d7f0b12e236417592bcfe8fcaa3a865
theboymo <billfero@gmail.com>
2018-02-10 17:29:56 +0000

    ensure access to projects is only to authorised users

--- everydayrails-rspec-2017/spec/controllers/projects_controller_spec.rb
+++ everydayrails-rspec-2017/spec/controllers/projects_controller_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe ProjectsController, type: :controller do
 	describe '#index' do
 
 		context 'as an authenticated user' do
-
 			before {
 				# simulate user login, access '#index' as an authenticated user
 				sign_in @user
@@ -29,6 +28,7 @@ RSpec.describe ProjectsController, type: :controller do
 			before {
 				get :index
 			}
+
 			it 'returns a 302 response' do
 				expect(response).to have_http_status 302
 			end
@@ -37,5 +37,40 @@ RSpec.describe ProjectsController, type: :controller do
 				expect(response).to redirect_to	'/users/sign_in'
 			end
 		end
+
 	end
+
+	describe '#show' do
+		# as a logged in user, I can view my own projects
+		context 'as an authorised user' do
+			before {
+				@user = FactoryBot.create(:user)
+				@project = FactoryBot.create(:project, owner: @user)
+			}
+
+			it 'responds successfully' do
+				sign_in @user
+				get :show, params: {id: @project.id}
+
+				expect(response).to be_success
+			end
+		end
+
+		# as a logged in user, I cannot view other users projects
+		context 'as an unauthorised user' do
+			before {
+				@user = FactoryBot.create(:user)
+				other_user = FactoryBot.create(:user)
+				@project = FactoryBot.create(:project, owner: other_user)
+			}
+
+			it 'redirects to the dashboard' do
+				sign_in @user
+				get :show, params: {id: @project.id}
+
+				expect(response).to redirect_to root_path
+			end
+		end
+	end
+
 end
