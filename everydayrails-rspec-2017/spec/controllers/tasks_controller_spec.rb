2347cd511f4634dded0897bfd74898450224bf07
theboymo <billfero@gmail.com>
2018-02-10 20:52:18 +0000

    check that authenticated users can create tasks

--- everydayrails-rspec-2017/spec/controllers/tasks_controller_spec.rb
+++ everydayrails-rspec-2017/spec/controllers/tasks_controller_spec.rb
@@ -22,6 +22,41 @@ RSpec.describe TasksController, type: :controller do
 		end
 	end
 
+	describe '#create' do
+		before {
+			@new_task = {name: 'New task'}
+		}
 
+		context 'as an authenticated user' do
+			before {
+				sign_in @user
+			}
+
+			it 'responds with JSON formatted output' do
+				post :create, format: :json, params: {project_id: @project.id, task: @new_task}
+				expect(response.content_type).to eq 'application/json'
+			end
+
+			it 'adds a new task to the project' do
+				expect {
+					post :create, format: :json, params: {project_id: @project.id, task: @new_task}
+				}.to change(@project.tasks, :count).by(1)
+			end
+		end
+
+		context 'as an unauthenticated user' do
+			it 'does not succeed' do
+				post :create, format: :json, params: {project_id: @project.id, task: @new_task}
+				expect(response).to_not be_success
+			end
+
+			it 'does not create a new task' do
+				expect {
+					post :create, format: :json, params: {project_id: @project.id, task: @new_task}
+				}.to_not change(@project.tasks, :count)
+			end
+		end
+
+	end
 
 end
