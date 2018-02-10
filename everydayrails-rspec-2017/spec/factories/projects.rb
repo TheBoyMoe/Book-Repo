d1e8d1df5446a83ad9c32881a06b74109ddb42f8
theboymo <billfero@gmail.com>
2018-02-10 10:32:33 +0000

    define multiple project factories

--- everydayrails-rspec-2017/spec/factories/projects.rb
+++ everydayrails-rspec-2017/spec/factories/projects.rb
@@ -1,8 +1,32 @@
 FactoryBot.define do
+
   factory :project do
-    sequence(:name) {|i| "Project #{i}"}
+    sequence(:name){|i| "Project #{i}"}
 		description "Dummy description ......."
 		due_on 1.week.from_now
 		association :owner
-  end
+	end
+
+	# define multiple factories each with different names, representing the same class
+	factory :project_due_yesterday, class: Project do
+		sequence(:name){|i| "Test project #{i}"}
+		description "Sample project - due yesterday"
+		due_on 1.day_ago
+		association :owner
+	end
+
+	factory :project_due_today, class: Project do
+		sequence(:name){|i| "Test project #{i}"}
+		description "Sample project - due today"
+		due_on Date.current.in_time_zone
+		association :owner
+	end
+
+	factory :project_due_tomorrow, class: Project do
+		sequence(:name){|i| "Test project #{i}"}
+		description "Sample project - due tomorrow"
+		due_on 1.day.from_now
+		association :owner
+	end
+
 end
