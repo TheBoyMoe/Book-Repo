3212e2716aea0b0306f8371a8c58ff1e8291d002
theboymo <billfero@gmail.com>
2018-04-17 16:50:22 +0100

    test model and controller

--- Rails-Novice-to-Ninja/test/models/story_test.rb
+++ Rails-Novice-to-Ninja/test/models/story_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "is not valid without a link" do
+    s = Story.create(name: 'My test submission', link: nil)  
+    assert s.errors[:link].any?  
+    refute s.valid?
+  end
+
+  test "is valid with required attributes" do  
+    s = Story.create(
+      name: 'My test submission', 
+      link: 'http://www.testsubmission.com/'
+    )
+    assert s.valid?end
+  end
