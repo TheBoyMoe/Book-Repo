7f8ef5fd42640d8e2c795edc8b03d4e960100e0c
theboymo <billfero@gmail.com>
2018-04-17 17:17:01 +0100

    implement the vote model and associations

--- Rails-Novice-to-Ninja/db/schema.rb
+++ Rails-Novice-to-Ninja/db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416182122) do
+ActiveRecord::Schema.define(version: 20180417155238) do
 
   create_table "stories", force: :cascade do |t|
     t.string "name"
     t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "story_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
