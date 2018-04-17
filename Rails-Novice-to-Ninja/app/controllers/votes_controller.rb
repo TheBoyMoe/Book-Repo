9e798a30ed16433bfd9ab8dee2b30f5cb5c79d29
theboymo <billfero@gmail.com>
2018-04-17 19:08:13 +0100

    implement the votes controller, submit votes using ajax

---
+++ Rails-Novice-to-Ninja/app/controllers/votes_controller.rb
@@ -0,0 +1,6 @@
+class VotesController < ApplicationController
+  def create
+    @story =Story.find(params[:story_id])
+    @story.votes.create
+  end
+end
--- Rails-Novice-to-Ninja/config/routes.rb
+++ Rails-Novice-to-Ninja/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   # get '/stories/index'
   # get '/stories/new'
   # post '/stories', to: 'stories#create'
 
-  resources :stories
+  resources :stories do
+    resources :votes
+  end
+
   root to: 'stories#index'
 end
