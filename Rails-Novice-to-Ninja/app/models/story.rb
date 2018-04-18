957a5bd5435f6ab9520d5432ae37ec794303dc15
theboymo <billfero@gmail.com>
2018-04-18 10:26:19 +0100

    add voting history, update using ajax

--- Rails-Novice-to-Ninja/app/models/story.rb
+++ Rails-Novice-to-Ninja/app/models/story.rb
@@ -1,10 +1,18 @@
 class Story < ApplicationRecord
   validates :name, :link, presence: true
-  has_many :votes
+
+  # customise has_many association
+  has_many :votes do
+    # return the last 6 votes
+    def latest
+      order('id DESC').limit(6)
+    end
+  end
 
   # override the default to_param method returning a simplfied version of the name instead of the ID
   def to_param
     # convert anything thats not an alpha-numeric char in to a '-' and append to the ID
     "#{id}-#{name.gsub(/\W/, '-').downcase}"
   end
+
 end
