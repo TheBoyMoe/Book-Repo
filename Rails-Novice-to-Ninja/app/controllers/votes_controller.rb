4e25c98a5407558adc0b1fd93bf1383ae558175e
theboymo <billfero@gmail.com>
2018-04-18 09:01:49 +0100

    implement user voting via ajax, with html fallback using remote: true and respond_to methods

--- Rails-Novice-to-Ninja/app/controllers/votes_controller.rb
+++ Rails-Novice-to-Ninja/app/controllers/votes_controller.rb
@@ -1,6 +1,14 @@
 class VotesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
-    @story =Story.find(params[:story_id])
-    @story.votes.create
+    @story = Story.find(params[:story_id])
+    @story.votes.create # vote consists of id and story_id
+    respond_to do |format|
+      # fallback if browser has js blocked, skip_before_action req'd otherwise fails due to invalid security token
+      format.html {redirect_to story_path(@story), notice: 'Vote was successfully cast.'}
+      format.js {} # renders app/views/votes/create.js.erb
+    end
   end
+
 end
