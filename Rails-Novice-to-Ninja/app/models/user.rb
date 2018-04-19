ccb3d98e59822b62bb104cfdbb35b764883430da
theboymo <billfero@gmail.com>
2018-04-19 10:01:16 +0100

    implement has_secure_password and the bcrypt gem

--- Rails-Novice-to-Ninja/app/models/user.rb
+++ Rails-Novice-to-Ninja/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # 1. adds .password and .password_confirmation methods
+  # adding validations to ensure the two match
+  # 2. encrypts the password, stored in the password_digest field
+  # 3. add the .authenticate instance method to the user model
+  # NOTE: requires the bycrypt gem
+  has_secure_password
 end
