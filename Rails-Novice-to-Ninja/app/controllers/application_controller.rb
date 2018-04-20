6791a71ae6c885f657aa629f2dc268792064a9b1
theboymo <billfero@gmail.com>
2018-04-20 13:17:31 +0100

    add ability for users to login and display their name

--- Rails-Novice-to-Ninja/app/controllers/application_controller.rb
+++ Rails-Novice-to-Ninja/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+
+  protected
+    def current_user
+      return unless session[:user_id]
+      # find returns an error if the user can't be found, where returns nil
+      @current_user = User.where(id: session[:user_id]).first
+    end
 end
