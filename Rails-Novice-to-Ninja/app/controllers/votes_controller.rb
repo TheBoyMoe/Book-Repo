46f3fe9ce94626ba1bdd5c8fb5d08b0de52aee7a
theboymo <billfero@gmail.com>
2018-04-21 18:03:56 +0100

    require login to vote

--- Rails-Novice-to-Ninja/app/controllers/votes_controller.rb
+++ Rails-Novice-to-Ninja/app/controllers/votes_controller.rb
@@ -1,13 +1,15 @@
 class VotesController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :ensure_login
 
   def create
     @story = Story.find(params[:story_id])
-    @story.votes.create(user: @current_user) # vote consists of id, user_id and story_id
-    respond_to do |format|
-      # fallback if browser has js blocked, skip_before_action req'd otherwise fails due to invalid security token
-      format.html {redirect_to story_path(@story), notice: 'Vote was successfully cast.'}
-      format.js {} # renders app/views/votes/create.js.erb
+    if @vote = @story.votes.create(user: @current_user) # vote consists of id, user_id and story_id
+      respond_to do |format|
+        # fallback if browser has js blocked, skip_before_action req'd otherwise fails due to invalid security token
+        format.html {redirect_to story_path(@story), notice: 'Vote was successfully cast.'}
+        format.js {} # renders app/views/votes/create.js.erb
+      end
     end
   end
 
