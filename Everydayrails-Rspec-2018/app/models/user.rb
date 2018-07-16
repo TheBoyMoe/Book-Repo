15e8dfe7457fec19f3b9c47adb29acccf68c3d7b
theboymo <billfero@gmail.com>
2018-07-16 14:55:33 +0100

    implement rspec tests on the user model

--- Everydayrails-Rspec-2018/app/models/user.rb
+++ Everydayrails-Rspec-2018/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # email & password validation provided by Devise
   validates :first_name, presence: true
   validates :last_name, presence: true
 
