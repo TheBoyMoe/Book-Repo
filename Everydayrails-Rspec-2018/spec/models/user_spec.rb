0abd6e23943f6fbee7922d93aefb2f4bc7f796b3
theboymo <billfero@gmail.com>
2018-07-17 08:32:20 +0100

    refactor user specs

--- Everydayrails-Rspec-2018/spec/models/user_spec.rb
+++ Everydayrails-Rspec-2018/spec/models/user_spec.rb
@@ -1,34 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with a first name, last name, email, and password' do
-    user = User.new( first_name: 'Tom', last_name: 'Jones', email: 'tom@ex.com', password: 'password')
-    expect(user).to be_valid
-  end
-
-  it 'is invalid without a fist name' do
-    user = User.new(first_name: nil)
-    user.valid?
-    expect(user.errors[:first_name]).to include("can't be blank")
-  end
-
-  it 'is invalid without a last name' do
-    user = User.new(last_name: nil)
-    user.valid?
-    expect(user.errors[:last_name]).to include("can't be blank")
-  end
-
-  it 'is invalid without an email address' do
-    user = User.new(email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("can't be blank")
-  end
   
-  it 'is invalid with a duplicate email adddress' do
-    User.create( first_name: 'Tom', last_name: 'Jones', email: 'tom@ex.com', password: 'password')
-    user = User.new( first_name: 'Dick', last_name: 'Jones', email: 'tom@ex.com', password: 'password')
-    user.valid?
-    expect(user.errors[:email]).to include('has already been taken')
+  describe 'is valid' do
+    it 'with a first name, last name, email, and password' do
+      user = User.new( first_name: 'Tom', last_name: 'Jones', email: 'tom@ex.com', password: 'password')
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'is invalid' do
+    it 'without a fist name' do
+      user = User.create(first_name: nil)
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+  
+    it 'without a last name' do
+      user = User.create(last_name: nil)
+      expect(user.errors[:last_name]).to include("can't be blank")
+    end
+  
+    it 'without an email address' do
+      user = User.create(email: nil)
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+    
+    it 'with a duplicate email adddress' do
+      User.create( first_name: 'Tom', last_name: 'Jones', email: 'tom@ex.com', password: 'password')
+      user = User.create( first_name: 'Dick', last_name: 'Jones', email: 'tom@ex.com', password: 'password')
+      expect(user.errors[:email]).to include('has already been taken')
+    end
   end
 
   it "return's the user's full name as a string" do
