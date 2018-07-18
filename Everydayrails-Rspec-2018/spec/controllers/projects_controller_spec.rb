c1449854c15bd61f073c4b53486f225cb4e7f7ad
theboymo <billfero@gmail.com>
2018-07-18 18:37:36 +0100

    implement specs for projects new and edit actions

--- Everydayrails-Rspec-2018/spec/controllers/projects_controller_spec.rb
+++ Everydayrails-Rspec-2018/spec/controllers/projects_controller_spec.rb
@@ -174,7 +174,7 @@ RSpec.describe ProjectsController, type: :controller do
         @user = FactoryBot.create(:user)
         @project = FactoryBot.create(:project, owner: @user)
       end
-      
+
       it 'deletes a project' do
         sign_in @user
         expect {
@@ -189,7 +189,7 @@ RSpec.describe ProjectsController, type: :controller do
         other_user = FactoryBot.create(:user)
         @project = FactoryBot.create(:project, owner: other_user)
       end
-      
+
       it 'does not delete the project' do
         sign_in @user
         expect {
@@ -200,7 +200,7 @@ RSpec.describe ProjectsController, type: :controller do
       it 'redirects to the dashboard' do
         sign_in @user
         delete :destroy, params: { id: @project.id }
-        expect(response).to redirect_to root_path 
+        expect(response).to redirect_to root_path
       end
     end
 
@@ -216,13 +216,99 @@ RSpec.describe ProjectsController, type: :controller do
 
       it 'redirects to the sign-in page' do
         delete :destroy, params: { id: @project.id }
-        expect(response).to redirect_to '/users/sign_in'
+        expect(response).to redirect_to user_session_path
       end
 
       it 'does not delete the project' do
         expect {
           delete :destroy, params: { id: @project.id }
         }.to_not change(Project, :count)
+      end
+    end
+  end
+
+  describe '#new' do
+    context 'as an authenticated user' do
+      before do
+        @user = FactoryBot.create(:user)
+        sign_in @user
+        get :new
+      end
+
+      it 'responds with a 200 status' do
+        expect(response).to have_http_status '200'
+      end
+
+      it 'renders a new template' do
+        expect(response).to render_template 'new'
+      end
+    end
+
+    context 'as a guest' do
+      before do
+        get :new
+      end
+
+      it 'responds with a 302 status' do
+        expect(response).to have_http_status '302'
+      end
+
+      it 'redirects tot the sign-in page' do
+        expect(response).to redirect_to user_session_path
+      end
+    end
+  end
+
+  describe '#edit' do
+    before do
+      @user = FactoryBot.create(:user)
+      @project = FactoryBot.create(:project, owner: @user)
+    end
+
+    context 'as an authenticated user' do
+      context 'as an authorised user' do
+        before do
+          sign_in @user
+          get :edit, params: { id: @project.id }
+        end
+
+        it 'returns a response status of 200' do
+          expect(response).to have_http_status '200'
+        end
+
+        it 'renders the edit view' do
+          expect(response).to render_template(:edit)
+        end
+      end
+
+      context 'as an unauthorised user' do
+        before do
+          other_user = FactoryBot.create(:user)
+          sign_in other_user
+          get :edit, params: { id: @project.id }
+        end
+
+        it 'returns a response status of 302' do
+          expect(response).to have_http_status '302'
+        end
+
+        it 'redirects to the dashboard' do
+          expect(response).to redirect_to root_path
+        end
+      end
+    end
+
+    context 'as a guest' do
+      before do
+        get :edit, params: { id: @project.id }
+      end
+
+      it 'returns a response status of 302' do
+        expect(response).to have_http_status '302'
+      end
+
+      it 'redirects to the dashboard' do
+        expect(response).to redirect_to user_session_path
       end
     end
   end
