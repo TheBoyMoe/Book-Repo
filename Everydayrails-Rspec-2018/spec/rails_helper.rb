4601d32e6d6c3886f7652b34b6e8a84d32bfae04
theboymo <billfero@gmail.com>
2018-07-19 14:37:03 +0100

    implement sign-in and sign-out helper methods

--- Everydayrails-Rspec-2018/spec/rails_helper.rb
+++ Everydayrails-Rspec-2018/spec/rails_helper.rb
@@ -18,4 +18,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include RequestSpecHelper, type: :request
 end
