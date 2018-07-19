ff706e0068fe568689532807769b0396496ec3f2
theboymo <billfero@gmail.com>
2018-07-19 16:48:28 +0100

    expand project controller feature tests, add update project

--- Everydayrails-Rspec-2018/spec/features/projects_controller_spec.rb
+++ Everydayrails-Rspec-2018/spec/features/projects_controller_spec.rb
@@ -1,30 +1,51 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 RSpec.feature 'Projects', type: :feature do
-  scenario 'user creates a new project' do
-    user = FactoryBot.create(:user)
-
+  before do
+    @user = FactoryBot.create(:user)
+    
     visit root_path
     click_link 'Sign in'
-    fill_in 'Email', with: user.email
-    fill_in 'Password',	with: user.password
+    fill_in 'Email', with: @user.email
+    fill_in 'Password',	with: @user.password
     click_button 'Log in'
-
+  end
+  
+  scenario 'user creates a new project' do
     expect {
       click_link 'New Project'
       fill_in 'Name',	with: 'Test Project'
       fill_in 'Description',	with: 'Trying out Capybara'
       click_button 'Create Project'
-
+      
       expect(page).to have_content 'Project was successfully created'
       expect(page).to have_content 'Test Project'
-      expect(page).to have_content "#{user.name}"
-    }.to change(user.projects, :count).by(1)
+      expect(page).to have_content "#{@user.name}"
+    }.to change(@user.projects, :count).by(1)
   end
-
+  
   # scenario 'guest adds a project' do
   #   visit projects_path
   #   save_and_open_page # DEBUG
   #   click_link 'New Project'
   # end
+  
+  scenario 'user updates a project' do
+    project = FactoryBot.create(:project,
+      owner: @user,
+      name: 'Old Project Name',
+      description: 'Old Project Description'
+    )
+    expect {
+      click_link project.name
+      click_link 'Edit'
+      fill_in 'Name',	with: 'New Project Title'
+      fill_in 'Description',	with: 'New Project Description'
+      click_button 'Update Project'
+  
+      expect(page).to have_content 'New Project Title'
+      expect(page).to have_content 'New Project Description'
+      expect(page).to have_content "#{@user.name}"
+    }
+  end
 end
