18757029502039abac335cc98e5c88c6868a2175
theboymo <billfero@gmail.com>
2018-07-19 10:49:18 +0100

    setup selenium-webdriver to run in headless mode

--- Everydayrails-Rspec-2018/spec/support/capybara.rb
+++ Everydayrails-Rspec-2018/spec/support/capybara.rb
@@ -1,1 +1,5 @@
-Capybara.javascript_driver = :selenium_chrome
+# launch chrome browser
+# Capybara.javascript_driver = :selenium_chrome
+
+# headless mode
+Capybara.javascript_driver = :selenium_chrome_headless
