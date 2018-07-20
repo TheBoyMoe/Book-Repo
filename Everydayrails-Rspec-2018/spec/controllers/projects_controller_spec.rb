4f84fe2333accbbc5772bce2bf9711bb616f4f33
theboymo <billfero@gmail.com>
2018-07-20 17:00:20 +0100

    implement a shared context to share variable values between files

--- Everydayrails-Rspec-2018/spec/controllers/projects_controller_spec.rb
+++ Everydayrails-Rspec-2018/spec/controllers/projects_controller_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  include_context 'project setup'
+
   describe '#index' do
     context 'as an authenticated user' do
       before do
         # create user and simulate sign-in
-        user = FactoryBot.create(:user)
+        # user = FactoryBot.create(:user)
         sign_in(user)
       end
       
@@ -37,14 +39,14 @@ RSpec.describe ProjectsController, type: :controller do
 
   describe '#show' do
     context 'as an authorised user' do
-      before do
-        @user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: @user)
-      end
+      # before do
+      #   @user = FactoryBot.create(:user)
+      #   @project = FactoryBot.create(:project, owner: @user)
+      # end
 
       it 'responds successfully' do
-        sign_in @user
-        get :show, params: { id: @project.id }
+        sign_in user
+        get :show, params: { id: project.id }
         expect(response).to be_success
       end
     end
@@ -53,11 +55,11 @@ RSpec.describe ProjectsController, type: :controller do
     # you need to be authenticated to CRUD projects
     context 'as an unauthorised user' do
       before do
-        @user = FactoryBot.create(:user)
+        # @user = FactoryBot.create(:user)
         other_user = FactoryBot.create(:user)
         @project = FactoryBot.create(:project, owner: other_user)
         
-        sign_in @user
+        sign_in user
         get :show, params: { id: @project.id }
       end
 
@@ -73,27 +75,27 @@ RSpec.describe ProjectsController, type: :controller do
 
   describe '#create' do
     context 'as an authenticated user' do
-      before do
-        @user = FactoryBot.create(:user)
-      end
+      # before do
+      #   @user = FactoryBot.create(:user)
+      # end
 
       context 'with valid attributes' do
         it 'adds a project' do
           project_params = FactoryBot.attributes_for(:project)
-          sign_in @user
+          sign_in user
           expect {
             post :create, params: { project: project_params }
-          }.to change(@user.projects, :count).by(1)
+          }.to change(user.projects, :count).by(1)
         end
       end
 
       context 'with invalid attributes' do
         it 'does not add a project' do
           project_params = FactoryBot.attributes_for(:project, :invalid)
-          sign_in @user
+          sign_in user
           expect {
             post :create, params: { project: project_params }
-          }.to_not change(@user.projects, :count)
+          }.to_not change(user.projects, :count)
         end
       end
     end
@@ -116,28 +118,28 @@ RSpec.describe ProjectsController, type: :controller do
 
   describe '#update' do
     context 'as an authorized user' do
-      before do
-        @user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: @user)
-      end
+      # before do
+      #   @user = FactoryBot.create(:user)
+      #   @project = FactoryBot.create(:project, owner: @user)
+      # end
 
       it 'updates the project' do
         project_params = FactoryBot.attributes_for(:project, name: 'New Project Name')
-        sign_in @user
-        patch :update, params: { id: @project.id, project: project_params }
-        expect(@project.reload.name).to eq 'New Project Name'
+        sign_in user
+        patch :update, params: { id: project.id, project: project_params }
+        expect(project.reload.name).to eq 'New Project Name'
       end
     end
 
     # try and update the project of another user
     context 'as an unauthorized user' do
       before do
-        @user = FactoryBot.create(:user)
+        # @user = FactoryBot.create(:user)
         other_user = FactoryBot.create(:user)
         @project = FactoryBot.create(:project, owner: other_user, name: 'Same Old Name')
  
         project_params = FactoryBot.attributes_for(:project, name: 'New Project Name')
-        sign_in @user # you need to be authenticated
+        sign_in user # you need to be authenticated
         patch :update, params: { id: @project.id, project: project_params }
       end
 
@@ -153,7 +155,7 @@ RSpec.describe ProjectsController, type: :controller do
     # unauthenticated user
     context 'as a guest' do
       before do
-        project = FactoryBot.create(:project)
+        # project = FactoryBot.create(:project)
         project_params = FactoryBot.attributes_for(:project, name: 'New Project Name')
         patch :update, params: { id: project.id, project: project_params }
       end
@@ -170,35 +172,32 @@ RSpec.describe ProjectsController, type: :controller do
 
   describe '#destroy' do
     context 'as an authorized user' do
-      before do
-        @user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: @user)
-      end
-
       it 'deletes a project' do
-        sign_in @user
+        user = FactoryBot.create(:user)
+        project = FactoryBot.create(:project, owner: user)
+        sign_in(user)
         expect {
-          delete :destroy, params: { id: @project.id }
-        }.to change(@user.projects, :count).by(-1)
+          delete :destroy, params: { id: project.id }
+        }.to change(user.projects, :count).by(-1)
       end
     end
 
     context 'as an unauthorised user' do
       before do
-        @user = FactoryBot.create(:user)
+        # @user = FactoryBot.create(:user)
         other_user = FactoryBot.create(:user)
         @project = FactoryBot.create(:project, owner: other_user)
       end
 
       it 'does not delete the project' do
-        sign_in @user
+        sign_in user
         expect {
           delete :destroy, params: { id: @project.id }
         }.to_not change(Project, :count)
       end
 
       it 'redirects to the dashboard' do
-        sign_in @user
+        sign_in user
         delete :destroy, params: { id: @project.id }
         expect(response).to redirect_to root_path
       end
@@ -230,8 +229,8 @@ RSpec.describe ProjectsController, type: :controller do
   describe '#new' do
     context 'as an authenticated user' do
       before do
-        @user = FactoryBot.create(:user)
-        sign_in @user
+        # @user = FactoryBot.create(:user)
+        sign_in user
         get :new
       end
 
@@ -260,16 +259,16 @@ RSpec.describe ProjectsController, type: :controller do
   end
 
   describe '#edit' do
-    before do
-      @user = FactoryBot.create(:user)
-      @project = FactoryBot.create(:project, owner: @user)
-    end
+    # before do
+    #   @user = FactoryBot.create(:user)
+    #   @project = FactoryBot.create(:project, owner: @user)
+    # end
 
     context 'as an authenticated user' do
       context 'as an authorised user' do
         before do
-          sign_in @user
-          get :edit, params: { id: @project.id }
+          sign_in user
+          get :edit, params: { id: project.id }
         end
 
         it 'returns a response status of 200' do
@@ -285,7 +284,7 @@ RSpec.describe ProjectsController, type: :controller do
         before do
           other_user = FactoryBot.create(:user)
           sign_in other_user
-          get :edit, params: { id: @project.id }
+          get :edit, params: { id: project.id }
         end
 
         it 'returns a response status of 302' do
@@ -300,7 +299,7 @@ RSpec.describe ProjectsController, type: :controller do
 
     context 'as a guest' do
       before do
-        get :edit, params: { id: @project.id }
+        get :edit, params: { id: project.id }
       end
 
       it 'returns a response status of 302' do
