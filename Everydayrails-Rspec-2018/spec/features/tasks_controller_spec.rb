4033aa9ac3746b02df6cfa7d28e9677485212f5b
theboymo <billfero@gmail.com>
2018-07-20 09:53:21 +0100

    implement feature specs addd, edit and delete tasks

--- Everydayrails-Rspec-2018/spec/features/tasks_controller_spec.rb
+++ Everydayrails-Rspec-2018/spec/features/tasks_controller_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  scenario 'user toggles a task', js: true do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, name: 'RSpec tutorial', owner: user)
-    task = project.tasks.create!(name: 'Finish RSpec tutorial')
-
+  before do
+    @user = FactoryBot.create(:user)
+    @project = FactoryBot.create(:project, name: 'RSpec tutorial', owner: @user)
+    
     visit root_path
     click_link 'Sign in'
-    fill_in 'Email',	with: user.email
-    fill_in 'Password', with: user.password
+    fill_in 'Email',	with: @user.email
+    fill_in 'Password', with: @user.password
     click_button 'Log in'
+  end
+  
+  scenario 'user toggles a task', js: true do
+    task = @project.tasks.create!(name: 'Finish RSpec tutorial')
 
     click_link 'RSpec tutorial'
     check 'Finish RSpec tutorial'
@@ -22,5 +25,41 @@ RSpec.feature "Tasks", type: :feature do
 
     expect(page).to_not have_css "label#task#{task.id}.completed"
     expect(task.reload).to_not be_completed
+  end
+
+  scenario 'user creates a new task' do
+    expect {
+      click_link 'RSpec tutorial'
+      click_link 'Add Task'
+      fill_in 'task_name', with: 'New Project Task'
+      click_button 'Create Task'
+      expect(page).to have_content('New Project Task')
+    }.to change(@project.tasks, :count).by(1)
+  end
+
+  scenario 'user edits a task' do
+    click_link 'RSpec tutorial'
+    click_link 'Add Task'
+    fill_in 'task_name', with: 'New Project Task'
+    click_button 'Create Task'
+    within '.task' do
+      click_link 'Edit'
+    end
+    fill_in 'task_name', with: 'New Task Name'
+    click_button 'Update Task'
+    expect(page).to have_content('New Task Name')
+  end
+
+  scenario 'user deletes a task', js: true do
+    click_link 'RSpec tutorial'
+    click_link 'Add Task'
+    fill_in 'task_name', with: 'New Project Task'
+    click_button 'Create Task'
+    expect(page).to have_content('New Project Task')  
+    within '.task' do
+      click_link 'Delete'
+    end
+    page.accept_alert
+    expect(page).to_not have_content('New Project Task')
   end
 end
