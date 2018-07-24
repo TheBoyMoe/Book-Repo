c1bc727ff46502a54051805a2a24766191638804
theboymo <billfero@gmail.com>
2018-07-24 19:19:29 +0100

    setup rspec and capybara test libraries

--- Build-Rails-Saas-App/config/application.rb
+++ Build-Rails-Saas-App/config/application.rb
@@ -21,11 +21,16 @@ module BuildRailsSaasApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: true,
+        helper_specs: true,
+        routing_specs: false,
+        request_specs: false,
+        model_specs: true,
+        controller_specs: true
+    end
 
     # Don't generate system test files.
     config.generators.system_tests = nil
