0d6bf7b356727ad8997da07db223d3e87dc3964a
theboymo <billfero@gmail.com>
2018-07-25 19:29:36 +0100

    started  rails saas demo again using version 5.0.6

---
+++ Build-Rails-Saas-App-5.0.6/spec/rails_helper.rb
@@ -0,0 +1,29 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rspec'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  # filter any tests tagged 'slow'
+  config.filter_run_excluding slow: true
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
